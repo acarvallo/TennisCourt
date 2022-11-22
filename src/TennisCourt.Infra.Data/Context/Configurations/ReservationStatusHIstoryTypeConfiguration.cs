eba3972e96fb7f8743f150dc723ff2f4e4c69c7a
Anderson Carvallo <anderson.carvallo@imaginebeyond.com.br>
2022-11-22 07:51:48 -0300

    Adding ReservationStatusHIstoryTypeConfiguration

---
+++ src/TennisCourt.Infra.Data/Context/Configurations/ReservationStatusHIstoryTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TennisCourt.Domain.Models;
+using TennisCourt.Infra.Data.Context.Configurations.Base;
+
+namespace TennisCourt.Infra.Data.Context.Configurations
+{
+    public class ReservationStatusHIstoryTypeConfiguration : BaseEntityTypeConfiguration<ReservationStatusHistory>
+    {
+        public override void Configure(EntityTypeBuilder<ReservationStatusHistory> builder)
+        {
+            base.Configure(builder);
+            builder.Property(x => x.ReservationStatus).IsRequired();
+        }
+    }
+}
