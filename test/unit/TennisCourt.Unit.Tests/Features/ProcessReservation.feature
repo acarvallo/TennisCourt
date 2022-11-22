35b8544262759e2ca491cacf3468c0a17f19ff21
Anderson Carvallo <anderson.carvallo@imaginebeyond.com.br>
2022-11-22 11:30:09 -0300

    Add process reservation feature specs and steps

--- test/unit/TennisCourt.Unit.Tests/Features/ProcessReservation.feature
+++ test/unit/TennisCourt.Unit.Tests/Features/ProcessReservation.feature
@@ -3,7 +3,7 @@
 A short summary of the feature
 
 @tag1
-Scenario: [scenario name]
-	Given [context]
-	When [action]
-	Then [outcome]
+Scenario: ProcessReservationAvailableDate
+	Given selected date 01/10/2020
+	When date is available to reservation
+	Then process reservation returing a new reservation id.
--- test/unit/TennisCourt.Unit.Tests/StepsDefinitions/ProcessReservationStepDefinitions.cs
+++ test/unit/TennisCourt.Unit.Tests/StepsDefinitions/ProcessReservationStepDefinitions.cs
@@ -1,9 +1,28 @@
-﻿using TechTalk.SpecFlow;
+﻿using System;
+using TechTalk.SpecFlow;
 
 namespace TennisCourt.Unit.Tests.StepsDefinitions
 {
+
     [Binding]
     public sealed class ProcessReservationStepDefinitions
     {
+        private DateTime _selectedReservationDate;
+
+        [Given("selected date (.*)")]
+        public void GivenSelectedDate(DateTime date)
+        {
+            _selectedReservationDate = date;
+        }
+        [When("date is available to reservation")]
+        public void WhenDateAvailable()
+        {
+
+        }
+        [Then("process reservation returing a valid GUID reservation id")]
+        public void ThenReservationNewID()
+        {
+
+        }
     }
 }
