cb5a734647e3eec23863f6e413f1970e04eb0723
Anderson Carvallo <anderson.carvallo@imaginebeyond.com.br>
2022-11-22 21:58:51 -0300

    Add reservation canceling scenario

--- test/unit/TennisCourt.Unit.Tests/Features/CancelReservation.feature
+++ test/unit/TennisCourt.Unit.Tests/Features/CancelReservation.feature
@@ -3,7 +3,10 @@
 A short summary of the feature
 
 @tag1
-Scenario: [scenario name]
-	Given [context]
-	When [action]
-	Then [outcome]
+Scenario: CancelReservationWhenValidId
+	Given the id of a existing and active reservation 
+	When canceling is requested
+	Then resevation status should change to <reservationStatus>
+    Examples:
+	| reservationStatus |
+	| CANCELED          |
--- test/unit/TennisCourt.Unit.Tests/StepsDefinitions/CancelReservationStepDefinitions.cs
+++ test/unit/TennisCourt.Unit.Tests/StepsDefinitions/CancelReservationStepDefinitions.cs
@@ -1,9 +1,34 @@
-﻿using TechTalk.SpecFlow;
+﻿using System;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TennisCourt.Unit.Tests.API;
 
 namespace TennisCourt.Unit.Tests.StepsDefinitions
 {
     [Binding]
     public sealed class CancelReservationStepDefinitions
     {
+        private readonly ReservationAPI _reservationAPI;
+        Guid _validReservationId;
+        public CancelReservationStepDefinitions(ReservationAPI reservationAPI)
+        {
+            _reservationAPI = reservationAPI;
+        }
+        [Given("the id of a existing and active reservation")]
+        public async Task GivenValidReservationId()
+        {
+            var output = await _reservationAPI.ProcessReservation(DateTime.Now, 100);
+            _validReservationId = output.Data.ReservationId;
+        }
+        [When("canceling is requested")]
+        public async Task WhenCancelingIsRequested()
+        {
+
+        }
+        [Then("resevation status should change to (.*)")]
+        public void ThenReservationStatusShouldChange(string reservationStatus) 
+        { 
+        }
+
     }
 }
